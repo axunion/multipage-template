41812cd56803bfd6cbedd86c91ebc38369ee8d91
axunion <120321953+axunion@users.noreply.github.com>
2023-08-11 14:23:28 +0900

    Add SVG icon component

--- src/pages/main.ts
+++ src/pages/main.ts
@@ -2,11 +2,13 @@ import './style.css';
 import { DialogComponent } from '@/components/Dialog';
 import { FooterComponent } from '@/components/Footer';
 import { HeaderComponent } from '@/components/Header';
+import { SVGIcon } from '@/components/SVGIcon';
 
 const componentList: Record<string, CustomElementConstructor> = {
   'dialog-component': DialogComponent,
   'footer-component': FooterComponent,
   'header-component': HeaderComponent,
+  'svg-icon': SVGIcon,
 };
 
 for (const key in componentList) {
---
+++ src/services/icons.ts
@@ -0,0 +1,4 @@
+export const icons: { [key: string]: string } = {
+  hamburger:
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><path stroke-width="2" d="M10 13H30 M10 20H30 M10 27H30" /></svg>',
+};
